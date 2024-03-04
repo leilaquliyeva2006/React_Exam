739e9d6fe348631c0df21b53f0732b01d6076176
leila <leylag659@gmail.com>
2024-03-04 23:45:43 +0400

    edit.tsx

---
+++ src/routes/edit.tsx
@@ -0,0 +1,24 @@
+import { redirect, RouteProps } from "react-router-dom";
+import { updateTask } from "../tasks";
+import { useParams } from "react-router-dom";
+
+type ActionProps = RouteProps & { request: Request };
+
+async function action({ request }: ActionProps) {
+  const { taskId } = useParams<{ taskId: string | undefined }>();
+  if (taskId === undefined) {
+    return redirect("/"); 
+  }
+
+  const formData = await request.formData();
+  const updates = Object.fromEntries(formData) as Record<string, string>;
+
+  await updateTask(taskId, updates);
+
+  return redirect(`/tasks/${taskId}`);
+}
+
+
+
+export { action };
+
