486c5f43a658a913da27e4b524d5a5ca792013a5
leila <leylag659@gmail.com>
2024-03-05 00:03:51 +0400

    complete Task.tsx

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,3 +1,7 @@
+import { Form, NavLink, useFetcher } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { completeTask }  from '../redux/slice';
+import DeleteForm from './forms/DeleteForm';
 
 export type TaskType = {
     id: string;
@@ -6,3 +10,42 @@ export type TaskType = {
     description :string | undefined;
 };
 
+function Task({ task }: { task: TaskType }) {
+    return (
+        <>
+            <CheckBox task={task} />
+            <NavLink to={`tasks/${task.id}`}>
+                {task.title ? <>{task.title}</> : <i>No Title</i>}
+                {''}
+            </NavLink>
+            <Form action={`tasks/${task.id}/edit`}>
+                <button type="submit">Edit</button>
+            </Form>
+            <DeleteForm task={task} />
+        </>
+    );
+}
+
+function CheckBox({ task }: { task: TaskType }) {
+    const fetcher = useFetcher();
+    const dispatch = useDispatch();
+    let isCompleted = task.completed;
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        dispatch(completeTask({ id: task.id }));
+    };
+
+    return (
+        <fetcher.Form method="post" action={`tasks/${task.id}/complete`}>
+            <button
+                name="isCompleted"
+                value={isCompleted ? 'true' : 'false'}
+                onClick={handleClick}
+            >
+                {isCompleted ? '▣' : '▢'}
+            </button>
+        </fetcher.Form>
+    );
+}
+
+export default Task;
